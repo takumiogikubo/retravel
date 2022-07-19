d9a9a630646ba83a7b39ff17454c67cf2d0c0400
takumiogikubo <ogi0926t@Gmail.com>
2022-07-19 14:05:32 +0000

    [add]deviseの導入（顧客・管理者）

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+config.hosts << "498a5297a02d4522b18f7164798f32db.vfs.cloud9.us-east-1.amazonaws.com"
 end
