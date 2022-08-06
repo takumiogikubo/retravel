ba6f7409772b22a719b766d7f512b6d3e3bc788a
takumiogikubo <ogi0926t@Gmail.com>
2022-08-06 04:39:45 +0000

    [uodate]フォロー一覧、フォロワー一覧作成

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -6,20 +6,20 @@ class Public::RelationshipsController < ApplicationController
    following = current_customer.follow(@customer)
    if following.save
      flash[:success] = 'ユーザーをフォローしました'
-     redirect_to @customer
+     redirect_back fallback_location: root_path
    else
      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-     redirect_to @customer
+     redirect_back fallback_location: root_path
    end
  end
  def destroy
    following = current_customer.unfollow(@customer)
    if following.destroy
      flash[:success] = 'ユーザーのフォローを解除しました'
-     redirect_to @customer
+     redirect_back fallback_location: root_path
    else
      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-     redirect_to @customer
+     redirect_back fallback_location: root_path
    end
  end
 
