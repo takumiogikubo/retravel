4afcbb916500431d2c5b16d5a91d32c1d12d2338
takumiogikubo <ogi0926t@gmail.com>
2022-09-02 17:00:32 +0000

    [update]アプリケーションの復元

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-config.hosts << "498a5297a02d4522b18f7164798f32db.vfs.cloud9.us-east-1.amazonaws.com"
+config.hosts << "ca4bcf2c97444329b86726b83eda0d75.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
